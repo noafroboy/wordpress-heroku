bc456bfd6a57661d9b7c89292389ddc40485e3af
Kevin Liu <kevin.liu@gmail.com>
2015-11-22 18:21:13 -0800

    Add linkedin sharing

--- wp-content/themes/grandblog/templates/template-share.php
+++ wp-content/themes/grandblog/templates/template-share.php
@@ -8,6 +8,7 @@
 <div id="social_share_wrapper">
 	<ul>
 		<li><a class="tooltip" title="<?php esc_html_e( 'Share On Facebook', 'grandblog-translation' ); ?>" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>"><i class="fa fa-facebook marginright"></i></a></li>
+		<li><a class="tooltip" title="Share On LinkedIn" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>"><i class="fa fa-linkedin marginright"></i></a>
 		<li><a class="tooltip" title="<?php esc_html_e( 'Share On Twitter', 'grandblog-translation' ); ?>" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?php echo get_permalink(); ?>&url=<?php echo get_permalink(); ?>"><i class="fa fa-twitter marginright"></i></a></li>
 		<li><a class="tooltip" title="<?php esc_html_e( 'Share On Pinterest', 'grandblog-translation' ); ?>" target="_blank" href="http://www.pinterest.com/pin/create/button/?url=<?php echo urlencode(get_permalink()); ?>&media=<?php echo urlencode($pin_thumb[0]); ?>"><i class="fa fa-pinterest marginright"></i></a></li>
 		<li><a class="tooltip" title="<?php esc_html_e( 'Share On Google+', 'grandblog-translation' ); ?>" target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>"><i class="fa fa-google-plus marginright"></i></a></li>
